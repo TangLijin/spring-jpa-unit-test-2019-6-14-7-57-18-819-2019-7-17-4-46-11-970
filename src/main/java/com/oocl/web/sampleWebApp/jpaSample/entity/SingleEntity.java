faedf8a631299647a6c1018d6311bfe9d3dc7fbb
LOUIS TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGLO2@OOCL.COM>
2019-07-17 18:03:00 +0800

    text:should be able to create and fetch entity.

--- src/main/java/com/oocl/web/sampleWebApp/jpaSample/entity/SingleEntity.java
+++ src/main/java/com/oocl/web/sampleWebApp/jpaSample/entity/SingleEntity.java
@@ -1,15 +1,21 @@
 package com.oocl.web.sampleWebApp.jpaSample.entity;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class SingleEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+
     private Long id;
     private String name;
+
+    public SingleEntity() {
+    }
 
     public String getName() {
         return name;
--- src/main/java/com/oocl/web/sampleWebApp/jpaSample/repository/EntityRepository.java
+++ src/main/java/com/oocl/web/sampleWebApp/jpaSample/repository/EntityRepository.java
@@ -1,9 +1,9 @@
-package com.oocl.web.sampleWebApp.jpaSample.repository;
-
-import com.oocl.web.sampleWebApp.jpaSample.entity.SingleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-public interface EntityRepository extends JpaRepository<SingleEntity,Long> {
-}
+//package com.oocl.web.sampleWebApp.jpaSample.repository;
+//
+//import com.oocl.web.sampleWebApp.jpaSample.entity.SingleEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//
+//
+//public interface EntityRepository extends JpaRepository<SingleEntity,Long> {
+//}
